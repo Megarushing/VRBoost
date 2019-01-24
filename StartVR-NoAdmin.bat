9121e7805b63a1e36afdd793ac5796e344bcd958
Leandro Trindade <megarushing@gmail.com>
2019-01-24 17:14:34 -0200

    Update StartVR-NoAdmin.bat

--- StartVR-NoAdmin.bat
+++ StartVR-NoAdmin.bat
@@ -1,3 +1,4 @@
+setlocal EnableDelayedExpansion
 for /f "skip=3 tokens=1" %%i in ('TASKLIST') do (
 SET /A p = 0
 for %%a in (amdow.exe AMDRSServ.exe atieclxx.exe atiesrxx.exe RadeonSettings.exe VirtualDesktop.Service.ex svchost.exe sihost.exe fontdrvhost.exe explorer.exe cmd.exe conhost.exe tasklist.exe smss.exe csrss.exe wininit.exe lsass.exe lsm.exe winlogon.exe services.exe dwm.exe nvvsvc.exe nvtray.exe nvxdsync.exe NvTmru.exe daemonu.exe nvSCPAPISvr.exe LogMeIn.exe LogMeInRC.exe OVRServer_x64.exe OVRServer_x86.exe vivelink.exe) do (
